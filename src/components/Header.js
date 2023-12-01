bfba14df9ebdb57a0b2be5afba9b12b14f56c359
ShoyebWritesCode <mdsoyeb@iut-dhaka.edu>
2023-12-02 00:17:35 +0600

    Added Landing Page

---
+++ src/components/Header.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../App.css';
+
+const Header = () => {
+  return (
+    <header>
+      <div className="header-container">
+        <div className="brand-text">
+          <div className="brand-line">ABC</div>
+          <div className="brand-line">PRODUCTION</div>
+          <div className="brand-line">STUDIO</div>
+        </div>
+        <div className="center-text">
+          <div className="center-line .title-line1">Movie</div>
+          <div className="center-line">Production</div>
+          <div className="center-line">Studio</div>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/projects">Projects</Link>
+            </li>
+            <li>
+              <Link to="/services">Services</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+            <li>
+              <Link to="/journal">Journal</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
